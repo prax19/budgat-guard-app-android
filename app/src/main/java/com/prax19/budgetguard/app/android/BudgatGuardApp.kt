ec2ca19cd168e5e319f9a24b52e7649bb6823382
prax19 <fhcarpaty2@gmail.com>
2024-01-06 22:15:31 +0100

    Add base project files

---
+++ app/src/main/java/com/prax19/budgetguard/app/android/BudgatGuardApp.kt
@@ -0,0 +1,8 @@
+package com.prax19.budgetguard.app.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BudgetGuardApp: Application() {
+}
---
+++ app/src/main/java/com/prax19/budgetguard/app/android/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.prax19.budgetguard.app.android
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.prax19.budgetguard.app.android.ui.theme.BudgetGuardTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            BudgetGuardTheme {
+
+            }
+        }
+    }
+}
