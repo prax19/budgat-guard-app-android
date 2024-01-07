d5798b64b4837ecc5a9cdf5c23724da70b513691
prax19 <fhcarpaty2@gmail.com>
2024-01-07 21:23:35 +0100

    Remove unused code in `BudgetDetailsScreenViewModel`

--- app/src/main/java/com/prax19/budgetguard/app/android/presentation/BudgetScreen/BudgetDetailsScreenViewModel.kt
+++ app/src/main/java/com/prax19/budgetguard/app/android/presentation/BudgetScreen/BudgetDetailsScreenViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.prax19.budgetguard.app.android.api.BudgetGuardApi
-import com.prax19.budgetguard.app.android.data.dto.BudgetDTO
 import com.prax19.budgetguard.app.android.data.dto.BudgetOperationDTO
 import com.prax19.budgetguard.app.android.data.model.Budget
 import com.prax19.budgetguard.app.android.data.model.Operation
@@ -23,9 +22,6 @@ class BudgetDetailsScreenViewModel @Inject constructor(
 
     private val _budgetState = mutableStateOf(BudgetState())
     val budgetState: State<BudgetState> = _budgetState
-
-//    private val _operationsState = mutableStateOf(OperationsState())
-//    val operationsState: State<OperationsState> = _operationsState
 
     private val budgetId: Long? = null
 
@@ -81,30 +77,9 @@ class BudgetDetailsScreenViewModel @Inject constructor(
         }
     }
 
-//    fun getOperations(budgetId: Long) {
-//            try {
-//                _operationsState.value = operationsState.value.copy(isLoading = true)
-//                _operationsState.value = operationsState.value.copy(
-//                    operations = api.getBudgetOperations(
-//                        "Basic cGF0cnlrLnBpcm9nQG8zNjUudXMuZWR1LnBsOnBhc3N3b3Jk",
-//                        budgetId
-//                    ),
-//                    isLoading = false
-//                )
-//            } catch (e: Exception) {
-//                Log.e("BudgetDetailsScreenViewModel", "getBudgetOperations: ", e)
-//                _operationsState.value = operationsState.value.copy(isLoading = false)
-//            }
-//    }
-
     data class BudgetState(
         val budget: Budget? = null,
         val isLoading: Boolean = false
     )
-
-//    data class OperationsState(
-//        val operations: List<BudgetOperation>? = null,
-//        val isLoading: Boolean = false
-//    )
 
 }
