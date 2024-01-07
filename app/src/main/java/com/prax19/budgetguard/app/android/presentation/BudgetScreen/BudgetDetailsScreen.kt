c5bcd55f50f917834f6e18873cf79e2760b8668c
prax19 <fhcarpaty2@gmail.com>
2024-01-07 20:57:57 +0100

    Add `Operation` item composable

--- app/src/main/java/com/prax19/budgetguard/app/android/presentation/BudgetScreen/BudgetDetailsScreen.kt
+++ app/src/main/java/com/prax19/budgetguard/app/android/presentation/BudgetScreen/BudgetDetailsScreen.kt
@@ -1,15 +1,25 @@
 package com.prax19.budgetguard.app.android.presentation.BudgetScreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.prax19.budgetguard.app.android.data.model.Operation
@@ -39,9 +49,90 @@ fun BudgetDetailsScreen(
                     .padding(bottom = it.calculateBottomPadding())
                     .padding(top = it.calculateTopPadding())
             ) {
-                for(op: Operation in budget.operations)
-                    Text(op.id.toString() + " " + op.name + " " + op.value)
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    content = {
+                        items(budget.operations) { operation ->
+                            BudgetOperationItem(
+                                navController = navController,
+                                operation = operation)
+                        }
+                    }
+                )
             }
         }
     }
+}
+
+@Composable
+fun BudgetOperationItem(
+    navController: NavController,
+    operation: Operation
+) {
+
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.75f),
+                ) {
+                    Text(
+                        text = operation.name,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+//                    Text(
+//                        text = "Short transaction description. Short transaction description. Short transaction description. Short transaction description. Short transaction description. Short transaction description. Short transaction description.",
+//                        style = MaterialTheme.typography.bodySmall,
+//                        maxLines = 3
+//                    )
+                }
+                val color =
+                    if(operation.value < 0)
+                        MaterialTheme.colorScheme.error
+                    else
+                        MaterialTheme.colorScheme.primary
+                Text(
+                    text = "${"%.2f".format(operation.value)} zł",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = color
+                )
+            }
+            Divider(
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .padding(bottom = 4.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Category",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = "prax19 • 16.12.2018",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+
 }
