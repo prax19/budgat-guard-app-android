7b69080e95ef44b7c080fe058e3ff652c82ec945
prax19 <fhcarpaty2@gmail.com>
2024-01-08 03:35:57 +0100

    Update `AddEditBudgetOperationScreen` background composable

--- app/src/main/java/com/prax19/budgetguard/app/android/presentation/AddEditBudgetOperationScreen/AddEditBudgetOperationScreen.kt
+++ app/src/main/java/com/prax19/budgetguard/app/android/presentation/AddEditBudgetOperationScreen/AddEditBudgetOperationScreen.kt
@@ -9,22 +9,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.InputChip
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -32,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -63,122 +57,122 @@ fun AddEditOperationScreen(
             usePlatformDefaultWidth = false
         )
     ) {
-
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = "Add new operation")
-                    },
-                    actions = {
-                        TextButton(
-                            modifier = Modifier
-                                .focusRequester(saveButtonFocusRequester),
-                            onClick = {
-                                val valueFloat = if(selectedType == operationTypes[0])
-                                    value.toFloat() * -1
-                                else
-                                    value.toFloat()
-                                onSave(name, valueFloat)
-                            }
-                        ) {
-                            Text("save")
-                        }
-                    }
-                )
-            },
-            content = {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(bottom = it.calculateBottomPadding())
-                        .padding(top = it.calculateTopPadding())
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(16.dp)
-                    ) {
-
-                        Row {
-                            operationTypes.forEach {operationType ->
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    InputChip(
-                                        modifier = Modifier
-                                            .padding(4.dp),
-                                        selected = (operationType == selectedType),
-                                        onClick = { selectedType = operationType },
-                                        label = { Text(operationType) },
-                                    )
-                                }
-                            }
-                        }
-
-                        OutlinedTextField(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .focusRequester(nameFocusRequester)
-                                .padding(4.dp),
-                            value = name,
-                            onValueChange = {
-                                name = it
-                            },
-                            placeholder = {
-                                Text(text = "Your operation name")
-                            },
-                            singleLine = true,
-                            label = {
-                                Text(text = "Name")
-                            },
-                            keyboardOptions = KeyboardOptions(
-                                imeAction = ImeAction.Next
-                            ),
-                            keyboardActions = KeyboardActions(
-                                onAny = {
-                                    valueFocusRequester.requestFocus()
-                                }
-                            )
-                        )
-
-                        OutlinedTextField(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .focusRequester(valueFocusRequester)
-                                .padding(4.dp),
-                            value = value,
-                            onValueChange = {
-                                value = it
-                            },
-                            placeholder = {
-                                Text(text = "10.00")
-                            },
-                            singleLine = true,
-                            label = {
-                                Text(text = "Value")
-                            },
-                            suffix = {
-                                Text(text = "zł")
-                            },
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Decimal,
-                                imeAction = ImeAction.Done
-                            ),
-                            keyboardActions = KeyboardActions(
-                                onAny = {
-                                    val valueFloat = if(selectedType == operationTypes[0])
+        Card {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "Add new operation")
+                        },
+                        actions = {
+                            TextButton(
+                                modifier = Modifier
+                                    .focusRequester(saveButtonFocusRequester),
+                                onClick = {
+                                    val valueFloat = if (selectedType == operationTypes[0])
                                         value.toFloat() * -1
                                     else
                                         value.toFloat()
                                     onSave(name, valueFloat)
                                 }
+                            ) {
+                                Text("save")
+                            }
+                        }
+                    )
+                },
+                content = {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = it.calculateBottomPadding())
+                            .padding(top = it.calculateTopPadding())
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                        ) {
+
+                            Row {
+                                operationTypes.forEach { operationType ->
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        InputChip(
+                                            modifier = Modifier
+                                                .padding(4.dp),
+                                            selected = (operationType == selectedType),
+                                            onClick = { selectedType = operationType },
+                                            label = { Text(operationType) },
+                                        )
+                                    }
+                                }
+                            }
+
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .focusRequester(nameFocusRequester)
+                                    .padding(4.dp),
+                                value = name,
+                                onValueChange = {
+                                    name = it
+                                },
+                                placeholder = {
+                                    Text(text = "Your operation name")
+                                },
+                                singleLine = true,
+                                label = {
+                                    Text(text = "Name")
+                                },
+                                keyboardOptions = KeyboardOptions(
+                                    imeAction = ImeAction.Next
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    onAny = {
+                                        valueFocusRequester.requestFocus()
+                                    }
+                                )
                             )
-                        )
+
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .focusRequester(valueFocusRequester)
+                                    .padding(4.dp),
+                                value = value,
+                                onValueChange = {
+                                    value = it
+                                },
+                                placeholder = {
+                                    Text(text = "10.00")
+                                },
+                                singleLine = true,
+                                label = {
+                                    Text(text = "Value")
+                                },
+                                suffix = {
+                                    Text(text = "zł")
+                                },
+                                keyboardOptions = KeyboardOptions(
+                                    keyboardType = KeyboardType.Decimal,
+                                    imeAction = ImeAction.Done
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    onAny = {
+                                        val valueFloat = if (selectedType == operationTypes[0])
+                                            value.toFloat() * -1
+                                        else
+                                            value.toFloat()
+                                        onSave(name, valueFloat)
+                                    }
+                                )
+                            )
+                        }
                     }
                 }
-            }
-        )
-
+            )
+        }
     }
 
 }
