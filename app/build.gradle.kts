4f3c8ab90e4bf7729666d6be0c760ad2c856a14f
prax19 <fhcarpaty2@gmail.com>
2024-01-11 00:16:49 +0100

    Add missing dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.50")
