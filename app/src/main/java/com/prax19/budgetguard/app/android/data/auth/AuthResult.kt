b631f49beb3b3b10743c039486991d571ad702e6
prax19 <fhcarpaty2@gmail.com>
2024-01-13 20:46:07 +0100

    Fix signing up error crash

--- app/src/main/java/com/prax19/budgetguard/app/android/data/auth/AuthResult.kt
+++ app/src/main/java/com/prax19/budgetguard/app/android/data/auth/AuthResult.kt
@@ -2,8 +2,14 @@ package com.prax19.budgetguard.app.android.data.auth
 
 sealed class AuthResult<T>(val data: T? = null, val message: String? = null) {
 
-    class Authorized<T>(data: T? = null): AuthResult<T>(data)
-    class Unauthorized<T>: AuthResult<T>()
-    class Error<T>(message: String? = null): AuthResult<T>(message = message)
+    class Authorized<T>(
+        data: T? = null
+    ): AuthResult<T>(data)
+    class Unauthorized<T>(
+        message: String = "User not authorized!"
+    ): AuthResult<T>(message = message)
+    class Error<T>(
+        message: String = "Unknown error!"
+    ): AuthResult<T>(message = message)
 
 }
